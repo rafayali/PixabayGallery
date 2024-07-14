cfcd78adfc45c7189a55a86b24595c7381884113
Rafay Ali <rafayalidev@gmail.com>
2024-07-14 23:26:41 +0500

    tests: fix syntax errors after api change

--- app/src/test/java/com/rafay/gallery/HomeViewModelTests.kt
+++ app/src/test/java/com/rafay/gallery/HomeViewModelTests.kt
@@ -1,20 +1,24 @@
 package com.rafay.gallery
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import com.rafay.gallery.api.Images
 import com.rafay.gallery.api.PixabayApi
-import com.rafay.gallery.common.CoroutineTestRule
 import com.rafay.gallery.common.Event
 import com.rafay.gallery.common.State
 import com.rafay.gallery.common.TestLiveDataObserver
+import com.rafay.gallery.data.fakeImageApiResponse
 import com.rafay.gallery.screens.home.HomeItem
 import com.rafay.gallery.screens.home.HomeViewModel
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
 import org.junit.After
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -23,44 +27,35 @@ class HomeViewModelTests {
     @get:Rule
     val rule = InstantTaskExecutorRule()
 
-    @get:Rule
-    val coroutinesTestRule = CoroutineTestRule()
+    private val testDispatcher = StandardTestDispatcher()
 
     private val testStateObserver = TestLiveDataObserver<State<List<HomeItem>>>()
 
     private val testErrorObserver = TestLiveDataObserver<Event<HomeViewModel.Error>>()
 
-    @After
-    fun tearDown() {
-        testStateObserver.clear()
-        testErrorObserver.clear()
+    @Before
+    fun setup() {
+        Dispatchers.setMain(testDispatcher)
     }
 
     @Test
-    fun `state should emit loading and then success after successful response`() =
-        coroutinesTestRule.runBlockingTest {
-            coroutinesTestRule.testCoroutineDispatcher.pauseDispatcher()
+    fun `state should emit loading and then success after successful response`() = runTest {
+        val mockPixabayApi = mockk<PixabayApi>()
+        coEvery { mockPixabayApi.getImages(any()) } returns fakeImageApiResponse
 
-            val mockPixabayApi = mockk<PixabayApi>()
-            coEvery { mockPixabayApi.getImages(any()) } returns fakeGetImagesApiResult()
+        val viewModel = HomeViewModel(pixabayApi = mockPixabayApi)
+        viewModel.state.observeForever(testStateObserver)
 
-            val viewModel = HomeViewModel(pixabayApi = mockPixabayApi)
-            viewModel.state.observeForever(testStateObserver)
+        assert(testStateObserver.values.firstOrNull() is State.Loading)
 
-            assert(testStateObserver.values.firstOrNull() is State.Loading)
+        assert(testStateObserver.values.drop(1).firstOrNull() is State.Success)
 
-            coroutinesTestRule.testCoroutineDispatcher.resumeDispatcher()
-
-            assert(testStateObserver.values.drop(1).firstOrNull() is State.Success)
-
-            viewModel.state.removeObserver(testStateObserver)
-        }
+        viewModel.state.removeObserver(testStateObserver)
+    }
 
     @Test
     fun `state should emit loading and then retry when there is an exception on first api response`() =
-        coroutinesTestRule.testCoroutineDispatcher.runBlockingTest {
-            coroutinesTestRule.testCoroutineDispatcher.pauseDispatcher()
-
+        runTest {
             val mockPixabayApi = mockk<PixabayApi>()
             coEvery { mockPixabayApi.getImages(any()) } throws Exception()
 
@@ -69,45 +64,36 @@ class HomeViewModelTests {
 
             assert(testStateObserver.values.firstOrNull() is State.Loading)
 
-            coroutinesTestRule.testCoroutineDispatcher.resumeDispatcher()
-
             assert(testStateObserver.values.drop(1).firstOrNull() is State.Retry)
 
             viewModel.state.removeObserver(testStateObserver)
         }
 
     @Test
-    fun `should call pixabayApi getImages exactly once after creating viewModel`() =
-        coroutinesTestRule.runBlockingTest {
-            coroutinesTestRule.testCoroutineDispatcher.pauseDispatcher()
+    fun `should call pixabayApi getImages exactly once after creating viewModel`() = runTest {
+        val mockPixabayApi =
+            mockk<PixabayApi>(relaxed = true) {
+                coEvery { getImages(any()) } returns fakeImageApiResponse
+            }
 
-            val mockPixabayApi =
-                mockk<PixabayApi>(relaxed = true) {
-                    coEvery { getImages(any()) } returns fakeGetImagesApiResult()
-                }
+        val viewModel = HomeViewModel(mockPixabayApi)
 
-            val viewModel = HomeViewModel(mockPixabayApi)
+        coVerify(exactly = 1) { mockPixabayApi.getImages(any()) }
 
-            coroutinesTestRule.testCoroutineDispatcher.resumeDispatcher()
-
-            coVerify(exactly = 1) { mockPixabayApi.getImages(any()) }
-
-            viewModel.state.removeObserver(testStateObserver)
-        }
+        viewModel.state.removeObserver(testStateObserver)
+    }
 
     @Test
     fun `_state should emit success on first and any follow up results when server return 200`() =
-        coroutinesTestRule.runBlockingTest {
-            coroutinesTestRule.testCoroutineDispatcher.pauseDispatcher()
-
+        runTest {
             val mockPixabayApi =
                 mockk<PixabayApi>(relaxed = true) {
                     coEvery {
                         getImages(any())
                     } coAnswers {
-                        fakeGetImagesApiResult()
+                        fakeImageApiResponse
                     } coAndThen {
-                        fakeGetImagesApiResult()
+                        fakeImageApiResponse
                     }
                 }
 
@@ -117,8 +103,6 @@ class HomeViewModelTests {
                     error.observeForever(testErrorObserver)
                     loadMore()
                 }
-
-            coroutinesTestRule.testCoroutineDispatcher.resumeDispatcher()
 
             assert(testStateObserver.values.firstOrNull() is State.Loading)
             assert(testStateObserver.values.drop(1).firstOrNull() is State.Success)
@@ -131,34 +115,10 @@ class HomeViewModelTests {
             viewModel.error.removeObserver(testErrorObserver)
         }
 
-    private fun fakeGetImagesApiResult() =
-        Images(
-            1,
-            1,
-            listOf(
-                Images.Hit(
-                    1L,
-                    "",
-                    Images.Hit.Type.Photo,
-                    "",
-                    "",
-                    1,
-                    1,
-                    "",
-                    1,
-                    1,
-                    "",
-                    1,
-                    1,
-                    1,
-                    1,
-                    1,
-                    1,
-                    1,
-                    1,
-                    "1",
-                    "",
-                ),
-            ),
-        )
+    @After
+    fun tearDown() {
+        testStateObserver.clear()
+        testErrorObserver.clear()
+        Dispatchers.resetMain()
+    }
 }
---
+++ app/src/test/java/com/rafay/gallery/data/FakeImageApi.kt
@@ -0,0 +1,33 @@
+package com.rafay.gallery.data
+
+import com.rafay.gallery.api.Images
+
+val fakeImageApiResponse = Images(
+    1,
+    1,
+    listOf(
+        Images.Hit(
+            1L,
+            "",
+            Images.Hit.Type.Photo,
+            "",
+            "",
+            1,
+            1,
+            "",
+            1,
+            1,
+            "",
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            "1",
+            "",
+        ),
+    ),
+)
